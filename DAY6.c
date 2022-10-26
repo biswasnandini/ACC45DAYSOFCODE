3ec97fe71e27aa77e5b103b34c39849ee5594787
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2022-10-26 17:57:03 +0530

    Add files via upload

---
+++ DAY6.c
@@ -0,0 +1,24 @@
+//6.write a program to check whether a number is palindrome or not.
+#include<stdio.h>  
+int main()    
+{    
+    int n,r,sum=0,temp;    
+    printf("enter the number=");    
+    scanf("%d",&n);    
+    temp=n;    
+    while(n>0)    
+    {    
+        r=n%10;    
+        sum=(sum*10)+r;    
+        n=n/10;    
+    }           
+    if(temp==sum)    
+    {
+        printf("palindrome number ");
+    }        
+    else
+    {    
+        printf("not palindrome"); 
+    }      
+    return 0;  
+}
