95cdd87e958631638c8e0ed23ccaa9ab46e88cdc
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2022-11-04 19:12:52 +0530

    Add files via upload

---
+++ DAY15.c
@@ -0,0 +1,14 @@
+//15.Write a program to call a function multiple times.
+#include <stdio.h>
+
+// Create a function
+void myFunction() {
+  printf("I just got executed!\n");
+}
+
+int main() {
+  myFunction(); // call the function
+  myFunction(); // call the function
+  myFunction(); // call the function
+  return 0;
+}
