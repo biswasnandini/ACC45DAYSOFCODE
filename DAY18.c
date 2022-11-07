ac010ea4b39d9a3d04a1c0f20800a6313eb342ad
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2022-11-07 21:44:58 +0530

    Add files via upload

---
+++ DAY18.c
@@ -0,0 +1,24 @@
+//18.Write a C program to count the total number of even and odd numbers in the array.
+#include<stdio.h>
+//to define the size of the array
+#define SIZE 5
+int main()
+{
+    int arr[SIZE],i,ec=0,oc=0;
+    printf("Enter 5 numbers in the array:\n",SIZE);
+    for(i=0;i<=SIZE-1;i++){
+        scanf("%d",&arr[i]);
+        //if the number is divisible by 2 0r not
+        if(arr[i]%2==0){
+            //even
+            ec++;
+        }
+        else{
+            //odd
+            oc++;
+        }
+    }
+    printf("Total number of even numbers elements in the array%d\n",ec);
+    printf("Total number of odd numbers elements in the array%d\n",oc);
+    return 0;
+}
